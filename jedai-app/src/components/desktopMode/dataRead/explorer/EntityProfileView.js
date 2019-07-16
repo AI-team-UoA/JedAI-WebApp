47716bc3b20ea0503033a3e62f67dec0dcf8f57f
Giorgos <giwrgosmandi@gmail.com>
2019-07-16 10:53:28 +0300

    entity id

--- jedai-app/src/components/desktopMode/dataRead/explorer/EntityProfileView.js
+++ jedai-app/src/components/desktopMode/dataRead/explorer/EntityProfileView.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types';
 import Form from 'react-bootstrap/Form'
 
 
 class EntityProfileView extends Component {
     render() {
 
+        // The View of each entity profile
         const { entityUrl, attributes} = this.props.entity;
         const index = this.props.index;
         return (
             <div>
                 <div style={{marginBottom:"5px"}}> 
                     <span style={{display:'inline', marginRight:"200px"}} >
-                        <h5 style={{display:'inline'}} >Entity ID:</h5> {index} 
+                        <h5 style={{display:'inline'}} >Entity ID:</h5> {index+ ((this.props.page-1)*5)} 
                     </span>
                     <span><h5 style={{display:'inline'}}>Entity URL:</h5> {entityUrl} </span>
                     
@@ -27,6 +29,14 @@ class EntityProfileView extends Component {
         )
     }
 }
+
+
+EntityProfileView.propTypes = {
+    page: PropTypes.number.isRequired,
+    index: PropTypes.number.isRequired,
+    entity: PropTypes.object.isRequired
+}
+
 
 
 export default EntityProfileView;
