05ad09251722d1fc2d4fd41c5812254241ec855d
Giorgos <giwrgosmandi@gmail.com>
2019-07-16 16:34:10 +0300

    block building

--- jedai-app/src/components/desktopMode/BlockBuilding.js
+++ jedai-app/src/components/desktopMode/BlockBuilding.js
@@ -1,12 +1,104 @@
 import React, { Component } from 'react'
-
+import {Form, Row } from 'react-bootstrap/'
+import MethodsAndConf from './utilities/MethodsAndConf'
 class BlockBuilding extends Component {
+
+    constructor(...args) {
+        super(...args);
+        this.submitChange = this.submitChange.bind(this)
+        this.state = {
+            block_building : [
+                {
+                        name: "STANDARD_TOKEN_BUILDING",
+                        selected: false,
+                        conf_type: ""
+                },  
+                {
+                        name: "SORTED_NEIGHBORHOOD",
+                        selected: false,
+                        conf_type: ""
+                    },  
+                    {
+                        name: "SORTED_NEIGHBORHOOD_EXTENDED",
+                        selected: false,
+                        conf_type: ""
+                    },  
+                    {
+                        name: "Q_GRAMS_BLOCKING",
+                        selected: false,
+                        conf_type: ""
+                    },  
+                    {
+                        name: "Q_GRAMS_BLOCKING_EXTENDED",
+                        selected: false,
+                        conf_type: ""
+                    },  
+                    {
+                        name: "SUFFIX_ARRAYS_BLOCKING",
+                        selected: false,
+                        conf_type: ""
+                },  
+                {
+                        name: "SUFFIX_ARRAYS_BLOCKING_EXTENDED",
+                        selected: false,
+                        conf_type: ""
+                    },  
+                    {
+                        name: "LSH_SUPERBIT_BLOCKING",
+                        selected: false,
+                        conf_type: ""
+                    },  
+                    {
+                        name: "LSH_MINHASH_BLOCKING",
+                        selected: false,
+                        conf_type: ""
+                    } 
+            ]
+        }
+    }
+
+    submitChange(child_state){
+        
+        this.state.block_building.forEach((method_state) => {
+            if (method_state.name === child_state.name){
+                console.log(child_state.name + " " + child_state.selected+ " " + child_state.conf_type)
+            }
+            this.setState({
+                block_building: this.state.block_building.map(el => (el.name === child_state.name ? {...child_state} : el))
+              });
+        })
+    }
+
     render() {
         return (
             <div>
-               <h1>BlockBuilding</h1> 
+                <br/>
+                <div style={{marginBottom:"5px"}}> 
+                    <h1 style={{display:'inline', marginRight:"20px"}}>Block Building</h1> 
+                    <span className="workflow-desc" >Block Building clusters entities into overlapping blocks in a lazy manner that relies on unsupervised blocking keys: every token in an attribute value forms a key. Blocks are then extracted, based on its equality or on its similarity with other keys.</span>
+                </div>
+
+                <br/>
+                <hr style={{ color: 'black', backgroundColor: 'black', height: '5' }}/>
+                <br/>
+
+                <Form.Group as={Row} className="form-row" > 
+                    <Form.Label><h5>Select Block Building methods and Configurations</h5></Form.Label>
+                </Form.Group>
+                 
+                <MethodsAndConf submitChange={this.submitChange} name="STANDARD_TOKEN_BUILDING" label="Standard/Token Blocking" />
+                <MethodsAndConf submitChange={this.submitChange} name="SORTED_NEIGHBORHOOD" label="Sorted Neighborhood" />
+                <MethodsAndConf submitChange={this.submitChange} name="SORTED_NEIGHBORHOOD_EXTENDED" label="Extended Sorted Neighborhood" />
+                <MethodsAndConf submitChange={this.submitChange} name="Q_GRAMS_BLOCKING" label="Q-Grams Blocking" />
+                <MethodsAndConf submitChange={this.submitChange} name="Q_GRAMS_BLOCKING_EXTENDED" label="Extended Q-Grams Blocking" />
+                <MethodsAndConf submitChange={this.submitChange} name="SUFFIX_ARRAYS_BLOCKING" label="Suffix Arrays Blocking" />
+                <MethodsAndConf submitChange={this.submitChange} name="SUFFIX_ARRAYS_BLOCKING_EXTENDED" label="Extended Suffix Arrays Blocking" />
+                <MethodsAndConf submitChange={this.submitChange} name="LSH_SUPERBIT_BLOCKING" label="LSH SuperBit Blocking" />
+                <MethodsAndConf submitChange={this.submitChange} name="LSH_MINHASH_BLOCKING" label="LSH MinHash Blocking" />
+                
             </div>
         )
     }
 }
 export default BlockBuilding
+
---
+++ jedai-app/src/components/desktopMode/utilities/MethodsAndConf.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types';
+import {Form, Col, Row, Alert } from 'react-bootstrap/'
+
+
+class MethodsAndConf extends Component {
+
+    constructor(...args) {
+        super(...args);
+        this.state = {
+            name: this.props.name,
+            selected: false,
+            conf_type: "default"
+        }
+    }
+
+    onChange = (e) => {
+        if (e.target.name === "conf_type")
+            this.setState({conf_type: e.target.value}, () => (this.submitChange()))
+        else
+            this.setState({selected:  !this.state.selected},() => (this.submitChange()))
+    }
+
+    submitChange(){
+        this.props.submitChange(this.state)
+    }
+
+    render() {
+        return (
+            <div>
+                <Alert  variant="primary" style={{height:'45px', width:"65%", margin:'auto', marginBottom:'10px' }}>
+                    <Form.Group as={Row} className="form-row" > 
+                        <Col sm={1}>
+                            <input type="checkbox" name={this.props.name} onChange={this.onChange}/>
+                        </Col>
+
+                        <Col sm={4}>
+                            <Form.Label>{this.props.label}</Form.Label>
+                        </Col>
+                        <Form>
+                            <Col sm={3} style={{ display: 'flex', flexDirection: 'row', float:'right'}} >
+                             
+                            <Form.Check
+                                    type="radio"
+                                    label="Default"
+                                    name="conf_type"
+                                    value="default"
+                                    style={{marginRight:'5px'}}
+                                    onChange={this.onChange}   
+                                    disabled={this.state.selected === false}   
+                                    defaultChecked
+                                                
+                                />
+                                <Form.Check
+                                    type="radio"
+                                    label="Automated"
+                                    name="conf_type"
+                                    value="automated"
+                                    style={{marginRight:'5px'}}
+                                    onChange={this.onChange} 
+                                    disabled={this.state.selected === false}
+                                />
+                                <Form.Check
+                                    type="radio"
+                                    label="Manual"
+                                    name="conf_type"
+                                    value="manual"
+                                    style={{marginRight:'5px'}}
+                                    onChange={this.onChange} 
+                                    disabled={this.state.selected === false}
+                                />
+                            </Col>
+                        </Form>
+                    </Form.Group>   
+                </Alert>
+            </div>
+        )
+    }
+}
+
+
+MethodsAndConf.propTypes = {
+    label: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    submitChange: PropTypes.func.isRequired
+}
+export default MethodsAndConf
