c63b4c674051d30d030d9f6419c3ff73d534fe49
Giorgos <giwrgosmandi@gmail.com>
2019-07-17 17:13:33 +0300

    forgotten return stmt

--- jedai-app/src/components/workflowViews/dataRead/DataReader.js
+++ jedai-app/src/components/workflowViews/dataRead/DataReader.js
@@ -63,7 +63,6 @@ import AlertModal from '../utilities/AlertModal'
 
 
     isValidated(){
-        return true
         var isSet
         if(this.state.er_mode === "dirty") isSet = this.state.entity1_set !==null && this.state.groundTruth_set !==null
         else if(this.state.er_mode === "clean") isSet = this.state.entity1_set !==null && this.state.entity2_set !== null && this.state.groundTruth_set !==null
